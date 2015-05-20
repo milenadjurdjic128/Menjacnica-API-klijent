8e2a781d31886c2b0c5cad0a598febf683b2a66b
Milena <Milena@milena-hp>
2015-05-20 13:46:16 +0200

    Napravljena main metoda i pozvana metoda azurirajValute

---
+++ Menjacnica API klijent/src/rs/ac/bg/fon/main/Main.java
@@ -0,0 +1,17 @@
+package rs.ac.bg.fon.main;
+
+import rs.ac.bg.fon.update.AzuriranjeKursneListe;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		AzuriranjeKursneListe comm = new AzuriranjeKursneListe();
+		
+		comm.azurirajValute();
+		
+		
+	}
+
+}
